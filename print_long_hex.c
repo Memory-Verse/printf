0dfefdcdcb445289064d3bf8ea2d1a4827196863
Clinton <c0772323281@gmail.com>
2023-07-25 15:06:47 -0700

    prints a long decimal in hexadecimal

---
+++ print_long_hex.c
@@ -0,0 +1,44 @@
+#include "main.h"
+/**
+ * print_lhex - prints a long decimal in hexadecimal
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int print_lhex(va_list arguments, char *buf, unsigned int ibuf)
+{
+	long int int_enter, k, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
+
+	int_enter = va_arg(arguments, long int);
+	isnegative = 0;
+	if (int_enter == 0)
+	{
+		ibuf = buf_con(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_enter < 0)
+	{
+		int_enter = (int_enter * -1) - 1;
+		isnegative = 1;
+	}
+
+	binary = malloc(sizeof(char) * (64 + 1));
+	binary = bin_array(binary, int_enter, isnegative, 64);
+	hexadecimal = malloc(sizeof(char) * (16 + 1));
+	hexadecimal = hex_array(binary, hexadecimal, 0, 16);
+	for (first_digit = k = count = 0; hexadecimal[k]; k++)
+	{
+		if (hexadecimal[k] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = buf_con(buf, hexadecimal[k], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count);
+}
