bcd77c4f6452cd3a5b49943881b424962775d526
Gbagome Livingstone <memoryverse0@gmail.com>
2023-07-25 21:37:32 +0000

    print integer with plus symbol

---
+++ print_plus_int.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_int_sym - print integer with plus symbol
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int print_int_sym(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_enter;
+	unsigned int int_in, int_temp, i, div;
+
+	int_enter = va_arg(arguments, int);
+	if (int_enter < 0)
+	{
+		int_in = int_enter * -1;
+		ibuf = buf_con(buf, '-', ibuf);
+	}
+	else
+	{
+		int_in = int_enter;
+		ibuf = buf_con(buf, '+', ibuf);
+	}
+	int_temp = int_in;
+	div = 1;
+	while (int_temp > 9)
+	{
+		div *= 10;
+		int_temp /= 10;
+	}
+	for (i = 0; div > 0; div /= 10, i++)
+	{
+		ibuf = buf_con(buf, ((int_in / div) % 10) + '0', ibuf);
+	}
+	return (i + 1);
+}
