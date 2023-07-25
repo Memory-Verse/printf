6b236c17ab7de09d06232c6085cfcf161acf7698
Gbagome Livingstone <memoryverse0@gmail.com>
2023-07-25 21:32:40 +0000

    prints a decimal in hexadecimal

---
+++ print_hex.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_hexdec - prints a decimal in hexadecimal
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int print_hexdec(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_enter, k, isnegative, count, first_digit;
+	char *hexadecimal, *binary;
+
+	int_enter = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_enter == 0)
+	{
+		ibuf = buf_con(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_enter < 0)
+	{
+		int_enter = (int_enter * -1) - 1;
+		isnegative = 1;
+	}
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = bin_array(binary, int_enter, isnegative, 32);
+	hexadecimal = malloc(sizeof(char) * (8 + 1));
+	hexadecimal = hex_array(binary, hexadecimal, 0, 8);
+	for (first_digit = k = count = 0; hexadecimal[k]; k++)
+	{
+		if (hexadecimal[k] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = buf_con(buf, hexadecimal[k], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(hexadecimal);
+	return (count);
+}
