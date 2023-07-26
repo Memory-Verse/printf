db3c0eb933c7a8c7d9bdcc98f62e9e101313049d
Gbagome Livingstone <memoryverse0@gmail.com>
2023-07-26 01:43:33 +0000

    prints decimal number in octal

--- print_oct.c
+++ print_oct.c
@@ -1,40 +1,40 @@
 #include "main.h"
 
 /**
- * print_dec_oct - prints decimal number in octal
+ * print_oct - prints decimal number in octal
  * @arguments: input number
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
-int print_dec_oct(va_list arguments, char *buf, unsigned int ibuf)
+int print_oct(va_list arguments, char *buf, unsigned int ibuf)
 {
-	int int_enter, i, isnegative, count, first_digit;
+	int int_input, i, isnegative, count, first_digit;
 	char *octal, *binary;
 
-	int_enter = va_arg(arguments, int);
+	int_input = va_arg(arguments, int);
 	isnegative = 0;
-	if (int_enter == 0)
+	if (int_input == 0)
 	{
-		ibuf = buf_con(buf, '0', ibuf);
+		ibuf = handl_buf(buf, '0', ibuf);
 		return (1);
 	}
-	if (int_enter < 0)
+	if (int_input < 0)
 	{
-		int_enter = (int_enter * -1) - 1;
+		int_input = (int_input * -1) - 1;
 		isnegative = 1;
 	}
 	binary = malloc(sizeof(char) * (32 + 1));
-	binary = bin_array(binary, int_enter, isnegative, 32);
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
 	octal = malloc(sizeof(char) * (11 + 1));
-	octal = oct_array(binary, octal);
+	octal = fill_oct_array(binary, octal);
 	for (first_digit = i = count = 0; octal[i]; i++)
 	{
 		if (octal[i] != '0' && first_digit == 0)
 			first_digit = 1;
 		if (first_digit)
 		{
-			ibuf = buf_con(buf, octal[i], ibuf);
+			ibuf = handl_buf(buf, octal[i], ibuf);
 			count++;
 		}
 	}
