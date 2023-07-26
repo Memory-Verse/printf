4be1823a4ff35dfdb8ab18356124f93c1109f703
Gbagome Livingstone <memoryverse0@gmail.com>
2023-07-26 01:38:06 +0000

    concatenates the buffer characters

--- handl_buf.c
+++ handl_buf.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * buf_con - concatenates the buffer characters
+ * handl_buf - concatenates the buffer characters
  * @buf: buffer pointer
  * @c: charcter to concatenate
  * @ibuf: index of buffer pointer
  * Return: index of buffer pointer.
  */
-unsigned int buf_con(char *buf, char c, unsigned int ibuf)
+unsigned int handl_buf(char *buf, char c, unsigned int ibuf)
 {
 	if (ibuf == 1024)
 	{
