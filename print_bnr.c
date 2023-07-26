bbdde0296935d19bfc56832da55a626c94bf6428
Gbagome Livingstone <memoryverse0@gmail.com>
2023-07-26 01:51:37 +0000

    prints decimal in binary

--- print_bnr.c
+++ print_bnr.c
@@ -1,31 +1,31 @@
 #include "main.h"
 
 /**
- * print_dec_bnr - prints decimal in binary
+ * print_bnr - prints decimal in binary
  * @arguments: input string
  * @buf: buffer pointer
  * @ibuf: index for buffer pointer
  * Return: number of chars printed.
  */
-int print_dec_bnr(va_list arguments, char *buf, unsigned int ibuf)
+int print_bnr(va_list arguments, char *buf, unsigned int ibuf)
 {
-	int int_enter, count, i, first_one, isnegative;
+	int int_input, count, i, first_one, isnegative;
 	char *binary;
 
-	int_enter = va_arg(arguments, int);
+	int_input = va_arg(arguments, int);
 	isnegative = 0;
-	if (int_enter == 0)
+	if (int_input == 0)
 	{
-		ibuf = buf_con(buf, '0', ibuf);
+		ibuf = handl_buf(buf, '0', ibuf);
 		return (1);
 	}
-	if (int_enter < 0)
+	if (int_input < 0)
 	{
-		int_enter = (int_enter * -1) - 1;
+		int_input = (int_input * -1) - 1;
 		isnegative = 1;
 	}
 	binary = malloc(sizeof(char) * (32 + 1));
-	binary = bin_array(binary, int_enter, isnegative, 32);
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
 	first_one = 0;
 	for (count = i = 0; binary[i]; i++)
 	{
@@ -33,7 +33,7 @@ int print_dec_bnr(va_list arguments, char *buf, unsigned int ibuf)
 			first_one = 1;
 		if (first_one == 1)
 		{
-			ibuf = buf_con(buf, binary[i], ibuf);
+			ibuf = handl_buf(buf, binary[i], ibuf);
 			count++;
 		}
 	}
