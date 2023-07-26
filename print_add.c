27e42cc2e93eab6d763241ad9998dd3dae1d45ca
Gbagome Livingstone <memoryverse0@gmail.com>
2023-07-26 01:48:42 +0000

    prints the address of an input variable

--- print_add.c
+++ print_add.c
@@ -1,48 +1,48 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_address - prints the address of an input variable
+ * print_add - prints the address of an input variable
  * @arguments: input address.
  * @buf: buffer pointer.
  * @ibuf: index for buffer pointer
  *
  * Return: number of chars printed.
  */
-int print_address(va_list arguments, char *buf, unsigned int ibuf)
+int print_add(va_list arguments, char *buf, unsigned int ibuf)
 {
 	void *add;
-	long int int_enter;
-	int k, count, first_digit, isnegative;
+	long int int_input;
+	int i, count, first_digit, isnegative;
 	char *hexadecimal, *binary;
 	char nill[] = "(nil)";
 
 	add = (va_arg(arguments, void *));
 	if (add == NULL)
 	{
-		for (k = 0; nill[k]; k++)
-			ibuf = buf_con(buf, nill[k], ibuf);
+		for (i = 0; nill[i]; i++)
+			ibuf = handl_buf(buf, nill[i], ibuf);
 		return (5);
 	}
-	int_enter = (intptr_t)add;
+	int_input = (intptr_t)add;
 	isnegative = 0;
-	if (int_enter < 0)
+	if (int_input < 0)
 	{
-		int_enter = (int_enter * -1) - 1;
+		int_input = (int_input * -1) - 1;
 		isnegative = 1;
 	}
 	binary = malloc(sizeof(char) * (64 + 1));
-	binary = bin_array(binary, int_enter, isnegative, 64);
+	binary = fill_binary_array(binary, int_input, isnegative, 64);
 	hexadecimal = malloc(sizeof(char) * (16 + 1));
-	hexadecimal = hex_array(binary, hexadecimal, 0, 16);
-	ibuf = buf_con(buf, '0', ibuf);
-	ibuf = buf_con(buf, 'x', ibuf);
-	for (first_digit = k = count = 0; hexadecimal[k]; k++)
+	hexadecimal = fill_hex_array(binary, hexadecimal, 0, 16);
+	ibuf = handl_buf(buf, '0', ibuf);
+	ibuf = handl_buf(buf, 'x', ibuf);
+	for (first_digit = i = count = 0; hexadecimal[i]; i++)
 	{
-		if (hexadecimal[k] != '0' && first_digit == 0)
+		if (hexadecimal[i] != '0' && first_digit == 0)
 			first_digit = 1;
 		if (first_digit)
 		{
-			ibuf = buf_con(buf, hexadecimal[k], ibuf);
+			ibuf = handl_buf(buf, hexadecimal[i], ibuf);
 			count++;
 		}
 	}
